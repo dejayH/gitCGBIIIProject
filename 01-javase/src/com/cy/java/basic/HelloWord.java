9951f01eba29422ac61fee6df2140c10c999e2e4
dejayH <dejayh@163.com>
2021-01-25 16:45:32 +0800

    JAVA

---
+++ 01-javase/src/com/cy/java/basic/HelloWord.java
@@ -0,0 +1,7 @@
+package com.cy.java.basic;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ 01-javase/src/com/cy/java/oop/ObjectTests.java
@@ -0,0 +1,8 @@
+package com.cy.java.oop;
+
+public class ObjectTests {
+    public static void main(String[] args) {
+        System.out.println("object");
+        System.out.println("yusout");
+    }
+}
