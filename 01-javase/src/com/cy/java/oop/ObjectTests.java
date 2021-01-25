78d393d6b3192d062b05c2ace8c4668443c4799b
dejayH <75826127+dejayH@users.noreply.github.com>
2021-01-25 15:07:33 +0800

    Update ObjectTests.java

--- 01-javase/src/com/cy/java/oop/ObjectTests.java
+++ 01-javase/src/com/cy/java/oop/ObjectTests.java
@@ -3,5 +3,6 @@ package com.cy.java.oop;
 public class ObjectTests {
     public static void main(String[] args) {
         System.out.println("object");
+        System.out.println("yusout");
     }
 }
