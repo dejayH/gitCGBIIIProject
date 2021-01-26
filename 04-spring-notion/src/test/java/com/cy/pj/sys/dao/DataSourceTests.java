81d60dfd625b659c83ed0d33039a502ca341ba0f
dejayH <dejayh@163.com>
2021-01-26 17:21:51 +0800

    spring test

---
+++ 04-spring-notion/src/test/java/com/cy/pj/sys/dao/DataSourceTests.java
@@ -0,0 +1,11 @@
+package com.cy.pj.sys.dao;
+
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * @springBootTest 注解描述的类
+ * 为springboot中的单元测试类
+ */
+
+@SpringBootTest
+public class DataSourceTests {
+}
